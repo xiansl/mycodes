741530c3ce9086a15e5f39910040e746c95b0207
xiansl <shang1982@gmail.com>
2018-01-03 22:55:13 +1100

    restructure

---
+++ crackingcodinginterview/making_anagrams.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int number_needed(string a, string b) {
+    int c1[26] = {0}, c2[26] = {0}, ct=0;
+    for(char& c : a) c1[c-'a']++;
+    for(char& c : b) c2[c-'a']++;
+    for(int i=0; i<26; i++) ct += abs(c1[i]-c2[i]);
+    return ct;
+}
+
+int main(){
+    string a;
+    cin >> a;
+    string b;
+    cin >> b;
+    cout << number_needed(a, b) << endl;
+    return 0;
+}
